a9b966bf8f366b1be103bfff825daf291cb4fcab
pyk <x24870@gmail.com>
2017-11-28 01:37:13 +0800

    collatz sequence

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-def test():
-    print("THis is test project")
+def collatz_sequence():
+    num = input("Please enter an positive integer\n")
     
+    print(num.isdigit())
     
-test()
+    while not num.isdigit():
+        num = input("Invalid input, please re-enter.\n")
+    else:
+        print("Input number is: ", num)
+        num = int(num)
+        
+    while num > 1:
+        if num % 2:
+            num = num * 3 + 1
+        else:
+            num /= 2
+        print(num)
+
+
+collatz_sequence()            
+    
