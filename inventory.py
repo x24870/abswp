1c53c073256db44520857d528b126c744361b0c8
Rick <Rick_Ye@pegatroncorp.com>
2017-12-01 13:04:04 +0800

    add inventory.py

---
+++ inventory.py
@@ -0,0 +1,14 @@
+stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+
+def print_inventory():
+    for k, v in stuff.items():
+        print(k + ":" + str(v))
+
+def add_stuff(key, add_value):
+    if key not in stuff:
+        stuff.setdefault(key, add_value)
+        print("new item")
+    else:
+        print(key+"origin num:" + str(stuff[key]) + ", after add:" + str(stuff[key]+add_value))
+        stuff[key] += add_value
+
