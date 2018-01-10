ac2ad72b24e77ea0c0dc6db4dfc97a93903d5ce2
Rick <Rick_Ye@pegatroncorp.com>
2018-01-10 14:45:21 +0800

    Finish randomQuizGenerator.py

--- ch8/randomQuizGenerator.py
+++ ch8/randomQuizGenerator.py
@@ -1,5 +1,6 @@
 import random
 
+
 capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
    'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver',
    'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee',
@@ -20,13 +21,32 @@ capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
     
     
 for quizNum in range(5):
-    quizFile = open('quiz%s.txt' %(quizNum + 1), 'w')
-    
+    quizFile = open('quiz%s.txt' % (quizNum + 1), 'w')
+    ansFile = open('ans%s.txt' % (quizNum +1), 'w')    
+
     #Quiz header
     quizFile.write('Name:\nNumber:\nDate:\n\n')
-    quizFile.write(' ' * 20 + 'Geography Quiz Form %s' % (quizNum + 1))
+    quizFile.write(' ' * 20 + 'Geography Quiz Form %s\n\n\n' % (quizNum + 1))
     
-stLst = list(capitals.keys())
-print(stLst)
-random.shuffle(stLst)
-print(stLst)
+    #Shuffle states
+    states = list(capitals.keys())
+    random.shuffle(states)
+
+    #write questions and answers
+    for quesNum in range(50):
+        correctAns = capitals[states[quesNum]]
+        wrongAns = list(capitals.values())
+        del wrongAns[wrongAns.index(correctAns)]
+        wrongAns = random.sample(wrongAns, 3)
+        ansOptions = wrongAns + [correctAns]
+        random.shuffle(ansOptions)
+
+        quizFile.write('%s. Which is the capital of %s?\n' % (quesNum+1, states[quesNum]))
+        for option in range(len(ansOptions)):
+            quizFile.write(' ' +  'ABCD'[option] + '. ' + ansOptions[option] + '\n')
+        quizFile.write('\n')
+
+        ansFile.write('%s %s\n' % (quesNum + 1, 'ABCD'[ansOptions.index(correctAns)]))
+
+    quizFile.close()
+    ansFile.close()
