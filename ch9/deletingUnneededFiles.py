b2f9827acb1bc3e687cb056930ebd8ceb55f9b73
pyk <x24870@gmail.com>
2018-01-17 21:33:23 +0800

    Finished deletingUnneededFiles.py

--- ch9/deletingUnneededFiles.py
+++ ch9/deletingUnneededFiles.py
@@ -1,17 +1,18 @@
 # Search current folder and subfolders, delete the files those size more than
 # 100 MB.
 
-import os, shutil
+import os
 
 def delNneedFiles(folder):
-    for foldername, subfolders, filenames in os.walk(folder):
-        print(foldername)
-        print(subfolders)
-        print(filenames)
+    absfolder = os.path.abspath(folder)
+    
+    for foldername, subfolders, filenames in os.walk(absfolder):
         for filename in filenames:
-            info = filename
-
-
+            abspath = os.path.join(foldername, filename)
+            size = os.path.getsize(abspath)
+            if size > 102400000:
+                print('***' + abspath + ' will be delete.')
+                os.unlink(abspath)
 
 
 if __name__ == '__main__':
